4b3ca066db15dbe3d0175089001d7758f29fe271
Ian Teo <ian_teo@msn.com>
2017-10-22 18:59:08 +0800

    Fix problem of duplicate papers in json (#19)

--- src/main/java/logic/command/WebCommand.java
+++ src/main/java/logic/command/WebCommand.java
@@ -6,8 +6,13 @@ import java.util.Queue;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import model.Model;
 import model.Paper;
+
 
 public class WebCommand implements Command{
     public static final String COMMAND_WORD = "web";
@@ -42,27 +47,37 @@ public class WebCommand implements Command{
         JSONArray paperArray = new JSONArray();
         JSONArray edgeList = new JSONArray();
 
-        int currentLevel = 2;
+        int currentLevel = 1;
+        HashSet<String> visitedSet = new HashSet<>();
+
         Queue<Paper> paperQueue = new LinkedList<>();
         paperQueue.offer(basePaper);
-        JSONObject basePaperObj = basePaper.toJson();
-        basePaperObj.put("group", 1);
-        paperArray.add(basePaperObj);
+
+        // Add null to the end of every "round" to show a new level
         paperQueue.offer(null);
 
         while (!paperQueue.isEmpty() && currentLevel <= level) {
             Paper next = paperQueue.poll();
             if (next == null) {
+                // Encounter null means time to increment level
                 paperQueue.offer(null);
                 currentLevel++;
                 if (paperQueue.peek() == null) break; // 2 nulls means terminate
                 else continue;
             }
+
+            if (visitedSet.contains(next.getId())) continue;
+            visitedSet.add(next.getId());
+
+            JSONObject paperObj = next.toJson();
+            paperObj.put("group", currentLevel);
+            paperArray.add(paperObj);
+
+            // If we are at the last level, we do not want to add the new edges
+            if (currentLevel == level) continue;
+
             for (Paper citation : next.getInCitation()) {
                 paperQueue.offer(citation);
-                JSONObject citationObj = citation.toJson();
-                citationObj.put("group", currentLevel);
-                paperArray.add(citationObj);
 
                 JSONObject edge = new JSONObject();
                 edge.put("source", next.getId());
@@ -70,6 +85,7 @@ public class WebCommand implements Command{
                 edgeList.add(edge);
             }
         }
+
         JSONObject citationWeb = new JSONObject();
         citationWeb.put("nodes", paperArray);
         citationWeb.put("links", edgeList);
