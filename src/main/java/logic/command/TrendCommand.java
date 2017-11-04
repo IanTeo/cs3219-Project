eec09ffdc1c5aaf7fa69f31a338fbaf28c294c2b
Ian Teo <ian_teo@msn.com>
2017-11-04 14:40:08 +0800

    Enable filtering by venue or author

--- src/main/java/logic/command/TrendCommand.java
+++ src/main/java/logic/command/TrendCommand.java
@@ -1,10 +1,11 @@
 package logic.command;
 
+import model.Author;
 import model.Model;
 import model.Paper;
 import util.JsonUtil;
+import util.StringUtil;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -12,19 +13,30 @@ import java.util.stream.IntStream;
 public class TrendCommand implements Command{
     public static final String COMMAND_WORD = "trend";
     public static final String HELP = "Error: %s\nUsage: trend [startYear]-[endYear] [type]\n" +
-            "This command returns a JSON file representing the venue/author/paper trend for the specified year range";
+            "This command returns a JSON file representing the venue/author trend for the specified year range";
     private Model model;
     private int startYear;
     private int endYear;
-    private String type;
+    private String seriesType;
 
     public String execute() {
-        // Map of (Venue Name -> Map of (Year -> Paper Count))
-        Map<String, Map<Integer, Integer>> map = new HashMap<>();
-        for (Paper paper : model.getPapers()) {
-            addToMap(map, paper);
+        // Map of (Series -> Map of (Year -> Paper Count))
+        Map<String, Map<Integer, Integer>> map;
+
+        switch (seriesType) {
+            case "venue" :
+                map = getVenueMap();
+                break;
+
+            case "author" :
+                map = getAuthorMap();
+                break;
+
+            default :
+                return String.format(HELP, "Invalid type");
         }
 
+        // Filters should happen here (If need to filter)
         filterTrivialData(map);
 
         return JsonUtil.toD3LineChartJson(map, getYearRange()).toString();
@@ -37,15 +49,38 @@ public class TrendCommand implements Command{
             String years[] = args[0].split("-");
             this.startYear = Integer.parseInt(years[0]);
             this.endYear = Integer.parseInt(years[1]);
-            this.type = args[1];
+            this.seriesType = StringUtil.parseString(args[1]);
         } catch (Exception e) {
             throw new Exception(String.format(HELP, "Error parsing parameters"));
         }
     }
 
+    private Map<String, Map<Integer, Integer>> getVenueMap() {
+        Map<String, Map<Integer, Integer>> map = new HashMap<>();
+        Map<String, String> actualNameMap = new HashMap<>();
+
+        for (Paper paper : model.getPapers()) {
+            actualNameMap.put(paper.getVenue().toLowerCase(), paper.getVenue());
+            addToMap(map, paper);
+        }
+        mapKeyToActualName(map, actualNameMap);
+
+        return map;
+    }
+
+    private Map<String, Map<Integer, Integer>> getAuthorMap() {
+        Map<String, Map<Integer, Integer>> map = new HashMap<>();
+
+        for (Author author : model.getAuthors()) {
+            addToMap(map, author);
+        }
+
+        return map;
+    }
+
     private void addToMap(Map<String, Map<Integer, Integer>> map, Paper paper) {
-        if (map.containsKey(paper.getVenue())) {
-            Map<Integer, Integer> yearMap = map.get(paper.getVenue());
+        if (map.containsKey(paper.getVenue().toLowerCase())) {
+            Map<Integer, Integer> yearMap = map.get(paper.getVenue().toLowerCase());
             if (isYearInRange(paper.getYear())) {
                 int count = yearMap.containsKey(paper.getYear()) ? yearMap.get(paper.getYear()) : 0;
                 yearMap.put(paper.getYear(), count + 1);
@@ -53,8 +88,19 @@ public class TrendCommand implements Command{
         } else {
             Map<Integer, Integer> yearMap = new HashMap<>();
             yearMap.put(paper.getYear(), 1);
-            map.put(paper.getVenue(), yearMap);
+            map.put(paper.getVenue().toLowerCase(), yearMap);
         }
+    }
+
+    private void addToMap(Map<String, Map<Integer, Integer>> map, Author author) {
+        Map<Integer, Integer> yearMap = new HashMap<>();
+        for (Paper paper : author.getPapers()) {
+            if (isYearInRange(paper.getYear())) {
+                int count = yearMap.containsKey(paper.getYear()) ? yearMap.get(paper.getYear()) : 0;
+                yearMap.put(paper.getYear(), count + 1);
+            }
+        }
+        map.put(author.getName(), yearMap);
     }
 
     private boolean isYearInRange(int year) {
@@ -71,21 +117,24 @@ public class TrendCommand implements Command{
 
         // Filter series with few data
         int yearRange = endYear - startYear + 1;
-        ArrayList<String> seriesToRemove = new ArrayList<>();
-        for (String series : map.keySet()) {
-            Map<Integer, Integer> yearMap = map.get(series);
-            int total = 0;
-            for (int count : yearMap.values()) {
-                total += count;
-            }
-            if (total < yearRange) {
-                seriesToRemove.add(series);
-            }
+        map.values().removeIf(yearMap -> seriesTotal(yearMap) < yearRange);
+    }
+
+    private int seriesTotal(Map<Integer, Integer> yearMap) {
+        int total = 0;
+        for (int count : yearMap.values()) {
+            total += count;
         }
+        return total;
+    }
 
-
-        for (String series : seriesToRemove) {
-            map.remove(series);
+    // Is there a better way to re-map the keys?
+    private void mapKeyToActualName(Map<String, Map<Integer, Integer>> map, Map<String, String> actualNameMap) {
+        for (String name : actualNameMap.keySet()) {
+            if (map.containsKey(name)) {
+                map.put(actualNameMap.get(name), map.get(name));
+                map.remove(name);
+            }
         }
     }
 }
